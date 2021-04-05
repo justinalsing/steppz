193ebb859653348c1265b0963b6f38bcb6d02464
Justin Alsing <j.a.alsing@gmail.com>
2021-04-05 11:47:21 +0200

    added double power-law model

---
+++ sps_models/model_B/sfh.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.integrate import cumtrapz
+
+# implement the Simha+2014 SFH model
+def sfh(tuniv, tau, alpha, beta, Z, nsteps=200):
+    
+    # time-grid
+    t = np.concatenate([tuniv * (1. - np.logspace(-3, -1e-10, nsteps))[::-1], np.array([tuniv])])
+
+    # compute sfr and normalize it
+    sfr = 1./((t/tau[i])**alpha[i] + (tau[i]/t)**beta[i])
+    sfr = sfr/np.trapz(x=t, y=sfr)
+
+    # compute the metallicity history based on the integral of the SFH
+    zt = np.concatenate([np.array([0.]), cumtrapz(x=t, y=sfr)])
+    
+    # re-normalize the metalicity history such that the mean metalicity over the last 10% of star formation = target Z
+    zt = zt * Z / zt[-1]
+        
+    return t, sfr*1e-9, zt
