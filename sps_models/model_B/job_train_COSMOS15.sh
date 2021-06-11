ca74d28a7cdbd30a5fb0b80f6bd9003ea58df4ba
Justin Alsing <justinalsing@Justins-MBP.lan>
2021-06-11 13:45:20 +0200

    updated job scripts

--- sps_models/model_B/job_train_COSMOS15.sh
+++ sps_models/model_B/job_train_COSMOS15.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH -J model_B_training_set
+#SBATCH -J train_B_COSMOS15
 #SBATCH -t 12:00:00
 #SBATCH -p cops
 #SBATCH --nodes=1
@@ -8,11 +8,9 @@
 
 ml unload conda/02
 source ~/.local-co/bin/setup-environment.sh
-ml prun
 
 ulimit -s unlimited  # Unlimited stack
 ulimit -u 16000      # Increase max number of tasks
 ulimit -n 65536      # Increase max number of open files
 
-prun python3 generate_spectra_mpi.py '/cfs/home/alju5794/steppz/sps_models/model_B/training_data/'
-prun python3 add_nebular_emission_mpi.py '/cfs/home/alju5794/steppz/sps_models/model_B/training_data/'
+python3 train_emulator_COSMOS15.py
