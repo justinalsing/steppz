195303c910a06991de1d13bfb335686497385de8
Justin Alsing <justinalsing@Justins-MBP.lan>
2021-06-23 11:10:58 +0200

    added first MCMC script

---
+++ kids/MCMC_HMII_baseline.py
@@ -0,0 +1,135 @@
+
+
+import numpy as np
+from speculator import Photulator, PhotulatorModelStack
+import time
+from affine import *
+
+import sys
+sys.path.append('/cfs/home/alju5794/steppz/code')
+from utils import *
+from priors import *
+from likelihoods import *
+
+# puffing factor for gaussian errors
+puff = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.3, 1.0, 1.0])
+
+# assumed fractional model error per band
+model_error = tf.constant([0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03, 0.03], dtype=tf.float32)
+
+# assumed ZP (fractional) error per band
+zp_error = tf.constant([0.05, 0.01, 0.01, 0.01, 0.03, 0.03, 0.03, 0.03, 0.03], dtype=tf.float32)
+
+# import data (with speczs)
+flux_sigmas = np.load('/cfs/home/alju5794/steppz/kids/data/flux_errors_no_missing.npy')*(1e9)*puff # units: nanomaggies
+fluxes = np.load('/cfs/home/alju5794/steppz/kids/data/fluxes_no_missing.npy')*1e9 # units: nanomaggies
+zspec = np.load('/cfs/home/alju5794/steppz/kids/data/zspec_no_missing.npy')
+zb = np.load('/cfs/home/alju5794/steppz/kids/data/zb_no_missing.npy')
+specsource = np.load('/cfs/home/alju5794/steppz/kids/data/specsource_no_missing.npy')
+
+# cut out dodgy values
+cut = (fluxes < 1e9).all(axis=1) * (zspec < 2.0) * (zspec > 1e-3) * (specsource != 'CDFS') * (specsource != 'VVDS')
+fluxes = fluxes[cut,:]
+flux_sigmas = flux_sigmas[cut,:]
+zspec = zspec[cut]
+zb = zb[cut]
+specsource = specsource[cut]
+
+# select a subset of the galaxies
+#fluxes = fluxes[0:n_galaxies:,:]
+#flux_sigmas = flux_sigmas[0:n_galaxies:,:]
+#zspec = zspec[0:n_galaxies:]
+#zb = zb[0:n_galaxies:]
+
+# convert to tensors
+flux_sigmas = tf.constant(np.atleast_2d(flux_sigmas).astype(np.float32), dtype=tf.float32)
+fluxes = tf.constant(np.atleast_2d(fluxes).astype(np.float32), dtype=tf.float32)
+
+# n_sigma_flux_cuts
+n_sigma_flux_cuts = tf.constant([1., 1., 3., 1., 0., 0., 0., 0., 0.], dtype=tf.float32)
+
+n_layers = 4
+n_hidden = 128
+filternames = ['omegacam_u', 'omegacam_g', 'omegacam_r', 'omegacam_i', 'VISTA_Z', 'VISTA_Y', 'VISTA_J', 'VISTA_H', 'VISTA_Ks']
+root_dir = '/cfs/home/alju5794/steppz/sps_models/model_HMII/trained_models/'
+filenames = ['model_{}x{}'.format(n_layers, n_hidden) + filtername for filtername in filternames]
+model = PhotulatorModelStack(root_dir=root_dir, filenames=filenames)
+
+# prior limits and associated hard priors
+sps_prior = priors.ModelHMIIBaselinePrior()
+n_sps_parameters = sps_prior.n_sps_parameters
+
+# bijector from constrained parameter (physical) to unconstrained space for sampling. Note: no bijector for the normalization parameter N
+# forward pass takes you from unconstrained space to physical parameter space
+bijector = sps_prior.bijector
+
+# declare any transforms that occur on the physical SPS parameters before passing to Speculator models
+transforms = [tfb.Identity() for _ in range(n_sps_parameters)]
+transforms[2] = tfb.Invert(tfb.Square()) # dust2 -> sqrt(dust2)
+transform = tfb.Blockwise(transforms)
+
+# input shape of latent parameters should be (n_walkers, n_galaxies, n_sps_parameters), output shape should be (n_walkers, n_galaxies)
+@tf.function
+def log_latentparameter_conditional(latentparameters, hyperparameters, fluxes, flux_variances, n_sigma_flux_cuts):
+    
+    # split the hyper parameters
+    zero_points, additive_fractional_errors = tf.split(hyperparameters, (n_bands, n_bands), axis=-1)
+    zero_points = tf.expand_dims(zero_points, axis=0)
+    additive_fractional_errors = tf.expand_dims(additive_fractional_errors, axis=0)
+
+    # convert latent parameters into physical parameters using bijector
+    theta = sps_prior.bijector(latentparameters)
+    N = theta[...,0] # extract normalization parameter N = -2.5log10M + dm(z)
+    
+    # model and predicted fluxes (multiplied by zero points)
+    model_fluxes = Emulator.fluxes(transform(theta[...,1:]), N)
+    predicted_fluxes = tf.multiply(model_fluxes, zero_points)
+    
+    # flux variances (quoted variances, scaled by zero points, plus additional fractional error terms)
+    predicted_flux_variances = tf.add(flux_variances, tf.square(tf.multiply(additive_fractional_errors, predicted_fluxes)))
+
+    # log likelihood
+    log_likelihood_ = log_likelihood_studentst2(fluxes, predicted_fluxes, predicted_flux_variances, n_sigma_flux_cuts)
+    
+    # log-prior
+    log_prior_ = sps_prior.log_prob(latentparameters)
+    
+    return log_likelihood_ + log_prior_
+
+# initial walker states
+n_walkers = 100
+latent_current_state = np.split(np.load('/cfs/home/alju5794/steppz/kids/initializations/HMII_walkers_phi.npy')[0:2*n_walkers,:,:], (2), axis=0)
+
+# initialize hyper-parameters
+hyperparameters = tf.concat([tf.ones(9, dtype=tf.float32), model_error + zp_error], axis=-1)
+
+# set up batching of latent parameters
+n_latent = fluxes.shape[0] # number of galaxies
+latent_batch_size = 10000
+n_latent_batches = n_latent // latent_batch_size + int( (n_latent % latent_batch_size) > 0)
+batch_indices = [np.arange(latent_batch_size*i, min(latent_batch_size*(i+1), n_latent)) for i in range(n_latent_batches)]
+
+# how many MCMC steps?
+n_steps = 300
+
+# timing benchmark
+batch = 0
+start = time.time()
+log_latentparameter_conditional(tf.gather(latent_current_state[0], batch_indices[batch], axis=1), hyperparameters, tf.gather(fluxes, batch_indices[batch], axis=0), tf.gather(flux_variances, batch_indices[batch], axis=0), n_sigma_flux_cuts)
+time_taken = time.time() - start()
+total_expected_time = (time_taken * 2 * n_steps * n_latent_batches) / (3600.)
+print('expected total runtime = {}hr'.format(time_taken * 2 * n_steps * n_latent_batches))
+
+# loop over batches
+for batch in range(n_latent_batches):
+    
+    # start timer
+    start = time.time()
+    
+    latent_samples = affine_sample_batch(log_latentparameter_conditional, n_steps, [tf.gather(latent_current_state[0], batch_indices[batch], axis=1), tf.gather(latent_current_state[1], batch_indices[batch], axis=1)], args=[hyperparameters, tf.gather(fluxes, batch_indices[batch], axis=0), tf.gather(flux_variances, batch_indices[batch], axis=0), n_sigma_flux_cuts])
+
+    # print time taken
+    print('batch {} took {}min'.format(batch, (time.time() - start)/60.))
+    
+    # save the chain
+    np.save('/cfs/home/alju5794/steppz/kids/chains/HMII_baseline_batch{}.npy'.format(batch), latent_samples.numpy())
