a78128d6d8c52777388c37a15c77b1d231b65028
Justin Alsing <justinalsing@Justins-MacBook-Pro.local>
2021-09-23 09:35:35 +0200

    updated likelihoods to catch nans in conditional sampling

--- code/likelihoods.py
+++ code/likelihoods.py
@@ -11,7 +11,7 @@ def log_likelihood_studentst2(fluxes, predicted_fluxes, predicted_flux_variances
     flux_cuts = tf.multiply(n_sigma_flux_cuts, predicted_flux_sigmas)
     
     # log-likelihood (Student's-t with 2 d.o.f)
-    log_likelihood_ = tf.reduce_sum(tf.add(tf.multiply(-1.5, tf.math.log(tf.add(1., tf.divide(tf.square(tf.subtract(predicted_fluxes, fluxes)), predicted_flux_variances)))), -tf.math.log(predicted_flux_sigmas) ), axis=-1)
+    log_likelihood_ = tf.reduce_sum(tf.add(tf.multiply(-1.5, tf.math.log(tf.add(1., 0.5 * tf.divide(tf.square(tf.subtract(predicted_fluxes, fluxes)), predicted_flux_variances)))), -tf.math.log(predicted_flux_sigmas) ), axis=-1)
     
     # selection cuts (CDF of the Student's-t for each band given the flux limits)
     log_selection_ = tf.reduce_sum(-tf.math.log(tf.subtract(0.5, tf.divide(tf.divide(tf.subtract(flux_cuts, predicted_fluxes), predicted_flux_sigmas), tf.multiply(tf.math.sqrt(8.), tf.math.sqrt(tf.add(1., tf.multiply(0.5, tf.square(tf.divide(tf.subtract(flux_cuts, predicted_fluxes), predicted_flux_sigmas))))))))), axis=-1)
